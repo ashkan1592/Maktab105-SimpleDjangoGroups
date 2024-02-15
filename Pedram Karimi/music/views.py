eb12b4119464fbe513d0390fe61e5f411b16b510
Pedram Karimi <147748351+pedramkarimii@users.noreply.github.com>
2024-02-16 00:46:22 +0330

    Update views.py Delete comment

--- Pedram Karimi/music/views.py
+++ Pedram Karimi/music/views.py
@@ -24,25 +24,6 @@ class InfoMusicView(ListView):
     error_message = "Error adding music"
 
 
-# class DeleteMusicView(DeleteView):
-#     model = MusicModel
-#     template_name = 'Musics/delete.html'
-#     success_url = reverse_lazy('info')
-#     success_message = "Music deleted successfully"
-#     error_message = "Error deleting music"
-#     context_object_name = 'music'
-#     pk_url_kwarg = 'pk'
-
-# def delete(self, request, *args, **kwargs):
-#     self.object = self.get_object()
-#     success_url = self.get_success_url()
-#     try:
-#         self.object.delete()
-#         messages.success(request, "Music deleted successfully")  # Display success message
-#     except Exception as e:
-#         messages.error(request, "Error deleting music: {}".format(str(e)))  # Display error message
-#     return HttpResponseRedirect(success_url)
-
 
 class DeleteMusicView(View):
     model = MusicModel
@@ -57,32 +38,14 @@ class DeleteMusicView(View):
         return redirect('info')
 
 
-#
-# class CountMusicView(DetailView):
-#     model = MusicModel
-#     template_name = 'Musics/info.html'
-#     context_object_name = 'music'
-#     # pk_url_kwarg = 'pk'
-# class CountMusicView(ListView):
-#     model = MusicModel
-#     template_name = 'Musics/count.html'
-#     context_object_name = 'music'
-#     success_message = "Music added successfully"
-#     error_message = "Error adding music"
-#     # pk_url_kwarg = 'pk'
-#     fields = '__all__'
-#     queryset = MusicModel.objects.all()
-#
 
 class CountMusicView(ListView):
-    template_name = 'Musics/count.html'  # Update with your template path
-    context_object_name = 'category_counts_data'  # Context variable name in the template
+    template_name = 'Musics/count.html'  
+    context_object_name = 'category_counts_data'  
 
     def get_queryset(self):
         return CategoryModel.objects.annotate(num_albums=Count('Album'))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Optionally, you can further customize the context data here
-        # For example, you might want to add additional context variables or perform additional queries
         return context
