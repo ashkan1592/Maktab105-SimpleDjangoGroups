7e9bec4453013cf3d50edc97e6d393b9bda518ed
pedramkarimii <pedram.9060@gmail.com>
2024-02-17 00:33:13 +0330

    Update Project Django Music

--- Pedram Karimi/core/models.py
+++ Pedram Karimi/core/models.py
@@ -1,7 +1,13 @@
+from uuid import uuid4
+
 from django.db import models
+from .managers import LogicalStatusMixin
 
 
-class DateTimeField(models.Model):
+class TimestampedModel(LogicalStatusMixin):
+    """
+    Abstract model providing fields for creation and update times.
+    """
     creat_time = models.DateField(auto_now_add=True, editable=False)
     update_time = models.DateField(auto_now=True, editable=False)
 
@@ -10,9 +16,19 @@ class DateTimeField(models.Model):
         ordering = ['-update_time']
 
 
-class BaseModel(models.Model):
-    id = models.BigAutoField(primary_key=True, editable=False, auto_created=True)
+class BaseModel(LogicalStatusMixin):
+    """
+    Abstract model with UUID primary key and a name field.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, auto_created=True)
     name = models.CharField(max_length=200)
+
+    @property
+    def title(self):
+        return self.name.title()
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
 
     class Meta:
         abstract = True
--- Pedram Karimi/music/views.py
+++ Pedram Karimi/music/views.py
@@ -1,12 +1,15 @@
 from django.contrib import messages
-from django.views.generic import CreateView, ListView, View
+from django.views.generic import CreateView, ListView, DeleteView, TemplateView
 from django.shortcuts import get_object_or_404, redirect
-from django.db.models import Count
 from django.urls import reverse_lazy
-from .models import MusicModel, CategoryModel
+from .models import MusicModel, CategoryModel, AlbumModel
+from core.managers import SoftDeleteQuerySet
 
 
 class AddMusicView(CreateView):
+    """
+    This view allows users to add new music entries.
+    """
     model = MusicModel
     fields = '__all__'
     success_url = reverse_lazy('home')
@@ -17,6 +20,9 @@ class AddMusicView(CreateView):
 
 
 class InfoMusicView(ListView):
+    """
+    This view displays information about existing music entries.
+    """
     model = MusicModel
     template_name = 'Musics/info.html'
     context_object_name = 'music'
@@ -24,28 +30,72 @@ class InfoMusicView(ListView):
     error_message = "Error adding music"
 
 
-
-class DeleteMusicView(View):
+class DeleteMusicByNameView(DeleteView, SoftDeleteQuerySet):
+    """
+    A view to delete a music object by its name.
+    Inherits from DeleteView and SoftDeleteQuerySet.
+    """
     model = MusicModel
     success_url = reverse_lazy('info')
+    template_name = 'Musics/delete.html'
 
-    def get(self, request, pk):
-        if isinstance(pk, int):
-            (music := get_object_or_404(self.model, pk=pk)) and music.delete()
-        else:
-            (music := get_object_or_404(self.model, title=pk)) and music.delete()
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests.
+        Deletes the music object(s) with the specified name from the database.
+        Adds a success message to inform the user.
+        Redirects the user to the success URL.
+        """
+        MusicModel.objects.filter(name=self.kwargs.get('pk')).delete()
         messages.success(request, 'Music deleted successfully', extra_tags='success')
-        return redirect('info')
+        return redirect(self.success_url)
 
 
+class DeleteMusicByIDView(DeleteView, SoftDeleteQuerySet):
+    """
+    A view to delete a music object by its ID.
+    Inherits from DeleteView and SoftDeleteQuerySet.
+    """
+    model = MusicModel
+    success_url = reverse_lazy('info')
+    template_name = 'Musics/delete.html'
 
-class CountMusicView(ListView):
-    template_name = 'Musics/count.html'  
-    context_object_name = 'category_counts_data'  
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests.
+        Deletes the music object(s) with the specified ID from the database.
+        Adds a success message to inform the user.
+        Redirects the user to the success URL.
+        """
+        MusicModel.objects.filter(name=self.kwargs.get('pk')).delete()
+        messages.success(request, 'Music deleted successfully', extra_tags='success')
+        return redirect(self.get_success_url())
 
-    def get_queryset(self):
-        return CategoryModel.objects.annotate(num_albums=Count('Album'))
+    def get_object(self, queryset=None):
+        """
+        Retrieves the music object by its primary key (ID).
+        """
+        pk = self.kwargs.get('pk')
+        return get_object_or_404(self.model, pk=pk)
+
+
+class CountMusicView(TemplateView):
+    """
+    This view displays counts of albums, categories, and music entries.
+    """
+    template_name = 'Musics/count.html'
 
     def get_context_data(self, **kwargs):
+        """
+        Add counts of albums, categories, and music entries to the context.
+        """
         context = super().get_context_data(**kwargs)
+        album_count = AlbumModel.objects.count()
+        category_count = CategoryModel.objects.count()
+        music_count = MusicModel.objects.count()
+
+        context['album_count'] = album_count
+        context['category_count'] = category_count
+        context['music_count'] = music_count
+
         return context
