c9f3b2ea154ac5879c7ce2075d99a3ea26171dd5
Reza Yazdani <mrrezayazdani@yahoo.com>
2024-02-17 08:30:35 +0330

    Delete Desktop directory

--- Desktop/New Folder (2)/fatemehaghaie/music/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from  .models import Music
-# Register your models here.
-@admin.register(Music)
-class MusicAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "category","cover","url")
--- Desktop/New Folder (2)/fatemehaghaie/music/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Music
-class Music_Form(forms.ModelForm):
-    class Meta:
-        model = Music
-        fields=["name", "category", "url","cover"]
-
